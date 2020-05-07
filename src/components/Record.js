0492470602a923da390edab3e37a521be95100d5
yangyipeng <yangyipeng@star-net.cn>
2020-05-07 21:34:06 +0800

    initial commit

--- src/components/Record.js
+++ src/components/Record.js
@@ -1,31 +1,75 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types'
+import {updateRecord, removeRecord} from '../utils/Api'
 
 export default class Record extends Component {
 
-    state = {}
+    state = {
+        edit: false
+    }
 
-    handleEdit = () => {
+    handleUpdate = () => {
+        const body = {
+            date: this.refs.date.value,
+            title: this.refs.title.value,
+            amount: Number.parseInt(this.refs.amount.value, 0)
+        }
 
+        updateRecord(this.props.record.id,body).then(
+            response => {
+                this.props.handleEditRecord(response.data);
+                this.handleToggle();
+            }
+        ).catch(error => {
+            console.log(error.message)
+        });
     }
 
     handleToggle = () => {
+        this.setState({edit: !this.state.edit})
+    }
 
+    handleDelete = () => {
+        removeRecord(this.props.record.id).then(
+            (response) => {
+                this.props.handleDeleteRecord(this.props.record)
+            }
+        ).catch((error) => {
+            console.log(error.message)
+        })
 
     }
 
     render() {
-        return (
-            <tr>
-                <td>{this.props.record.date}</td>
-                <td>{this.props.record.title}</td>
-                <td>{this.props.record.amount}</td>
-                <td>
-                    <button className="btn btn-info mr-1" onClick={this.handleEdit}>Update</button>
-                    <button className="btn btn-danger" onClick={this.handleToggle}>Cancel</button>
-                </td>
-            </tr>
-        )
+        if (this.state.edit) {
+            return (
+                <tr>
+                    <td><input type="text" className="form-control" defaultValue={this.props.record.date} ref="date"/>
+                    </td>
+                    <td><input type="text" className="form-control" defaultValue={this.props.record.title} ref="title"/>
+                    </td>
+                    <td><input type="text" className="form-control" defaultValue={this.props.record.amount}
+                               ref="amount"/></td>
+                    <td>
+                        <button className="btn btn-info mr-1" onClick={this.handleUpdate}>Update</button>
+                        <button className="btn btn-danger" onClick={this.handleToggle}>Cancel</button>
+                    </td>
+                </tr>
+            )
+        } else {
+            return (
+                <tr>
+                    <td>{this.props.record.date}</td>
+                    <td>{this.props.record.title}</td>
+                    <td>{this.props.record.amount}</td>
+                    <td>
+                        <button className="btn btn-info mr-1" onClick={this.handleToggle}>Edit</button>
+                        <button className="btn btn-danger" onClick={this.handleDelete}>Delete</button>
+                    </td>
+                </tr>
+            )
+        }
+
     }
 
 }
--- src/components/Records.js
+++ src/components/Records.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Record from "./Record";
 import {getRecords} from "../utils/Api";
 import RecordForm from "./RecordForm";
+import AmountBox from "./AmountBox";
 
 export default class Records extends Component {
 
@@ -13,23 +14,65 @@ export default class Records extends Component {
     }
 
     componentDidMount() {
-        this.setState({loading:true})
+        this.setState({loading: true})
         getRecords().then(res => {
             console.log(res);
-            this.setState({loading:false,records:res.data})
+            this.setState({loading: false, records: res.data})
         }).catch(error => {
             console.log(error);
-            this.setState({loading:false,error})
+            this.setState({loading: false, error})
         })
     }
 
 
-    addRecord=(record)=>{
+    addRecord = (record) => {
         this.setState({
-            error:null,
-            loading:false,
-            records:[...this.state.records,record]
+            error: null,
+            loading: false,
+            records: [...this.state.records, record]
         })
+    }
+
+    handleDeleteRecord = (record) => {
+        const records = this.state.records.filter((item, index) => {
+            return record.id !== item.id
+        })
+        this.setState({records})
+    }
+
+    handleEditRecord = (record) => {
+        const records = this.state.records.map((item, index) => {
+            if (record.id !== item.id) {
+                return item;
+            }
+            return {...item, ...record}
+        })
+        this.setState({records})
+
+    }
+
+    credits() {
+        let credits = this.state.records.filter((record) => {
+            return record.amount >= 0;
+        })
+
+        return credits.reduce((prev, curr) => {
+            return prev + Number.parseInt(curr.amount, 0)
+        }, 0)
+    }
+
+    debits() {
+        let credits = this.state.records.filter((record) => {
+            return record.amount < 0;
+        })
+
+        return credits.reduce((prev, curr) => {
+            return prev + Number.parseInt(curr.amount, 0)
+        }, 0)
+    }
+
+    balance() {
+        return this.credits() + this.debits();
     }
 
     render() {
@@ -38,24 +81,29 @@ export default class Records extends Component {
         let recordsComponent;
 
         if (loading) {
-            recordsComponent=<h2>Loading</h2>
+            recordsComponent = <h2>Loading</h2>
         } else if (error) {
-            recordsComponent=<div>{error.message}</div>
-        }else {
-            recordsComponent=(
-                    <table className="table table-bordered">
-                        <thead>
-                        <tr>
-                            <th>Data</th>
-                            <th>Title</th>
-                            <th>Amount</th>
-                            <th>Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {records.map((record,index)=> <Record key={record.id} record={record}/>)}
-                        </tbody>
-                    </table>
+            recordsComponent = <div>{error.message}</div>
+        } else {
+            recordsComponent = (
+                <table className="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Title</th>
+                        <th>Amount</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {records.map((record, index) =>
+                        <Record key={record.id} record={record}
+                                handleDeleteRecord={this.handleDeleteRecord}
+                                handleEditRecord={this.handleEditRecord}
+
+                        />)}
+                    </tbody>
+                </table>
             )
         }
 
@@ -63,6 +111,11 @@ export default class Records extends Component {
         return (
             <div className={'container'}>
                 <h2>Records</h2>
+                <div className="row mb-3">
+                    <AmountBox text="Credit" type="success" amount={this.credits()}/>
+                    <AmountBox text="Debit" type="danger" amount={this.debits()}/>
+                    <AmountBox text="Balance" type="info" amount={this.balance()}/>
+                </div>
                 <RecordForm handleNewRecord={this.addRecord}/>
                 {recordsComponent}
             </div>
